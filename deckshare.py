634f7867b23a615cd8bafe64f9adfdb1b6abf80c
ningxiaoxiao <732869825@qq.com>
2019-08-20 20:50:06 +0800

    更新

--- deckshare.py
+++ deckshare.py
@@ -10,10 +10,11 @@ from urllib.request import urlretrieve
 
 if os.path.exists("C:/deckshare/pic") ==False:
     os.mkdir("C:/deckshare/pic")
-
+if os.path.exists("C:/deckshare/cards") ==False:
+    os.mkdir("C:/deckshare/cards")
 allcards = json.load(open('cards.json', encoding='utf-8'))
 
-code="AADEdQ0pdsoBgphvLx_YL1f5QsseFVBZOKZfj5LACuiTpLw"
+
 
 #把网站返回的数据转成数组
 def toCardArray(jdata):
@@ -65,47 +66,56 @@ def downloadCardImg(url, filename):
     urlretrieve('http:' + url, fileurl)
     return fileurl
 
-jdata=getCards(code)
-cards=toCardArray(jdata)
+def getdeck(name,clan,code):
+    jdata=getCards(code)
+    cards=toCardArray(jdata)
 
-xls = open('c:/deckshare/cards.csv','w',newline='',encoding='utf-8-sig')
-cw=csv.writer(xls)
-cw.writerow(['name','cost','count','pic'])
-for c in cards:
-    cw.writerow(c)
-#费用图
-costmap=[0,0,0,0,0,0,0,0,0]
-for c in cards:
-    cost=int(c[1])
-    count=int(c[2])
-    if cost==0:#0费计到1费中
-        costmap[0]+=count
-        continue
-    if cost > 9:#9和9费以上 者放到9中
-        costmap[8]+=count
-        continue
-    costmap[cost-1]+=count
-xls = open('c:/deckshare/deckshare.csv','w',newline='',encoding='utf-8-sig')
-cw=csv.writer(xls)
+    xls = open('c:/deckshare/cards.csv','w',newline='',encoding='utf-8-sig')
+    cw=csv.writer(xls)
+    cw.writerow(['name','cost','count','pic'])
+    for c in cards:
+        cw.writerow(c)
+    #费用图
+    costmap=[0,0,0,0,0,0,0,0,0]
+    for c in cards:
+        cost=int(c[1])
+        count=int(c[2])
+        if cost==0:#0费计到1费中
+            costmap[0]+=count
+            continue
+        if cost > 9:#9和9费以上 者放到9中
+            costmap[8]+=count
+            continue
+        costmap[cost-1]+=count
+    xls = open('c:/deckshare/deckshare.csv','w',newline='',encoding='utf-8-sig')
+    cw=csv.writer(xls)
+    #写标题
+    titles=[]
+    titles.append('name')
+    titles.append('class')
+    for x in range(1,19):
+        titles.append('pic'+str(x))
+    for x in range(1,10):
+        titles.append('cost'+str(x))
 
-titles=[]
-for x in range(1,19):
-    titles.append('pic'+str(x))
-for x in range(1,10):
-    titles.append('cost'+str(x))
 
-cw.writerow(titles)
+    cw.writerow(titles)
 
-row=[]
-for x in range(1,19):
-    row.append('c:/deckshare/cards/'+str(x)+'.png')
-for x in costmap:
-    row.append(x)
-cw.writerow(row)
-#清空cards
-for x in os.listdir("cards"):
-    os.remove('cards/'+x)
+    row=[]
+    row.append(name)
+    row.append('c:/deckshare/class/'+clan+'.png')
+    for x in range(1,19):
+        row.append('c:/deckshare/cards/'+str(x)+'.png')
+    for x in costmap:
+        row.append(x)
+    cw.writerow(row)
+    #清空cards
+    for x in os.listdir("cards"):
+        os.remove('cards/'+x)
 
-#这里要把少的图片用一个空图片补上,不然会出问题
-for x in range(len(cards),19):
-    shutil.copy('null.png','cards/'+str(x)+'.png')
+    #这里要把少的图片用一个空图片补上,不然会出问题
+    for x in range(len(cards),19):
+        shutil.copy('null.png','cards/'+str(x)+'.png')
+
+
+getdeck('Makiyui','吸血鬼','AADE0mf4S-vLDQzoJOa1JEET8Cu-ZEIAKktpzmdb-Beefjo')
